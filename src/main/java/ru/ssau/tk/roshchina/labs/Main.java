97f8ebb9d914a485f74991dd5e8c018755ac3496
roshchina-anna <roshchina.anna2@mail.ru>
2025-09-25 08:46:23 +0400

    first commit

--- src/main/java/ru/ssau/tk/roshchina/labs/Main.java
+++ src/main/java/ru/ssau/tk/roshchina/labs/Main.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.roshchina.labs;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println("Hello world");
     }
 }
