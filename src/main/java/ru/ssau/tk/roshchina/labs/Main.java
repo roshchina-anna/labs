1c813c2b7229dfa271945b254a01806f548b7619
roshchina-anna <roshchina.anna2@mail.ru>
2025-09-25 08:56:04 +0400

    Добавлен интерфейс MathFunction

--- src/main/java/ru/ssau/tk/roshchina/labs/Main.java
+++
@@ -1,6 +0,0 @@
-package ru.ssau.tk.roshchina.labs;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/main/java/ru/ssau/tk/roshchina/labs/functions/MathFunction.java
@@ -0,0 +1,5 @@
+package ru.ssau.tk.roshchina.labs.functions;
+
+interface MathFunction {
+    double apply(double x);
+}
