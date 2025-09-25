844dc244e6e6b6c8a786e0dd09a68f2b2b781bae
roshchina-anna <roshchina.anna2@mail.ru>
2025-09-25 22:42:35 +0400

    добавлен класс SqrFunction и тесты к нему

---
+++ src/main/java/ru/ssau/tk/roshchina/labs/functions/SqrFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.roshchina.labs.functions;
+
+public class SqrFunction implements MathFunction {
+    public double apply(double x) {
+        return Math.pow(x, 2);
+    }
+}
---
+++ src/test/java/ru/ssau/tk/roshchina/labs/functions/SqrFunctionTest.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.roshchina.labs.functions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrFunctionTest {
+    public void testSqr() {
+        SqrFunction sqrFunction = new SqrFunction();
+        assertEquals(0.0, sqrFunction.apply(0), 0.0001);
+        assertEquals(49.0, sqrFunction.apply(7.0), 0.0001);
+        assertEquals(2.25, sqrFunction.apply(-1.5), 0.0001);
+        assertEquals(12.6025, sqrFunction.apply(3.55), 0.0001);
+        assertEquals(0.0001, sqrFunction.apply(0.01), 0.0001);
+    }
+}
