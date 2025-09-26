0d8870b8393573ece4e7f28fde11f9404d377f7c
roshchina-anna <roshchina.anna2@mail.ru>
2025-09-26 21:55:42 +0400

    добавлен метод andThen в интерфейс MathFunctionи и тесты к нему

--- src/main/java/ru/ssau/tk/roshchina/labs/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/roshchina/labs/functions/MathFunction.java
@@ -2,4 +2,7 @@ package ru.ssau.tk.roshchina.labs.functions;
 
  public interface MathFunction {
     double apply(double x);
+     default MathFunction andThen(MathFunction afterFunction) {
+         return new CompositeFunction(this, afterFunction);
+     }
 }
